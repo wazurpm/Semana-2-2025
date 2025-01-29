1c2036cd513c6b6bb38abb2cbc26bf6c5710c511
Javier Carrillo <javier.60207@gmail.com>
2025-01-28 17:07:47 -0800

    resuelto

--- C - Matriz Hermosa.py
+++ C - Matriz Hermosa.py
@@ -7,10 +7,12 @@ def obtener_datos():
 def calcular_respuesta(matrix : list):
     ubicacion = []
     for i in range(len(matrix)):
-        if matrix[i].index(1):
-            ubicacion = [int(i), int(matrix[i].index("1"))]
-    respuesta = max(ubicacion) - min(ubicacion)
-    return respuesta
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == "1":
+                ubicacion = [abs((i+1)-3),abs((j+1)-3)]
+                print(ubicacion)
+                break
+    return max(ubicacion) + min(ubicacion)
 
 matriz = obtener_datos()
 print(calcular_respuesta(matriz))
