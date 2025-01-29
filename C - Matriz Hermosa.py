095bc226c69c8f1261ed3f18df2721929c528dcc
Javier Carrillo <javier.60207@gmail.com>
2025-01-28 17:12:11 -0800

    arreglado para pasar los tests automaticos

--- C - Matriz Hermosa.py
+++ C - Matriz Hermosa.py
@@ -10,9 +10,8 @@ def calcular_respuesta(matrix : list):
         for j in range(len(matrix[i])):
             if matrix[i][j] == "1":
                 ubicacion = [abs((i+1)-3),abs((j+1)-3)]
-                print(ubicacion)
                 break
-    return max(ubicacion) + min(ubicacion)
+    return sum(ubicacion)
 
 matriz = obtener_datos()
 print(calcular_respuesta(matriz))
