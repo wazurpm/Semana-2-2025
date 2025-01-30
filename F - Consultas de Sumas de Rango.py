6933bbe8a25f4f9d089642cbcc41ff9d25b0ce9c
Javier Carrillo <squitrpm@gmail.com>
2025-01-30 12:59:56 -0800

    terminados los metodos del problema F

--- F - Consultas de Sumas de Rango.py
+++ F - Consultas de Sumas de Rango.py
@@ -1,3 +1,17 @@
 def obtener_datos():
     cantidad_valores, cantidad_consultas = map(int, input().split())
-    
+    arreglo = list(map(int, input().split()))
+    for i in range(cantidad_consultas):
+        consultas.append(list(map(int, input().split())))
+
+def calcular_respuesta(arreglo, consultas : list):
+    for consulta in consultas:
+        sum(arreglo[consulta[0], consulta[1]])
+
+cantidad_valores = 0
+cantidad_consultas = 0
+arreglo = []
+consultas = []
+
+obtener_datos()
+calcular_respuesta()
